daa44cb62970a4b891cd402ce2257de8bcfc8a63
= <madhur2511@gmail.com>
2016-02-27 22:14:18 -0800

    Working Google Auth and MySQL Integration

--- apptest.py
+++ apptest.py
@@ -1,6 +1,6 @@
-from flask.ext.mysqldb import MySQL
+from flask import Flask, redirect, url_for, session, request, jsonify
 from flask_oauthlib.client import OAuth
-from flask import Flask, redirect, url_for, session, request, jsonify, json, render_template
+
 
 app = Flask(__name__)
 app.config['GOOGLE_ID'] = "554572499920-tgb7m9i8a8lbjvtbu0srrq50paq0oduj.apps.googleusercontent.com"
@@ -23,16 +23,8 @@ google = oauth.remote_app(
     authorize_url='https://accounts.google.com/o/oauth2/auth'
 )
 
-# MySQL configurations
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'BucketList'
-app.config['MYSQL_HOST'] = 'localhost'
 
-mysql = MySQL(app)
-
-
-@app.route("/")
+@app.route('/')
 def index():
     access_token = session.get('access_token')
     if access_token is None:
@@ -48,37 +40,19 @@ def index():
         res = urlopen(req)
     except URLError, e:
         if e.code == 401:
+            # Unauthorized - bad token
             session.pop('access_token', None)
             return redirect(url_for('login'))
         return res.read()
 
-    userInfo = res.read()
-    return updateDB(json.loads(userInfo))
-
-
-def updateDB(userInfo):
-    _email  = userInfo['email']
-    _name   = userInfo['name']
-
-    if _name and _email:
-        conn = mysql.connection
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM student WHERE name='%s'" % (_name))
-        data = cur.fetchall()
-
-        if len(data) is 0:
-            cur.execute("""INSERT INTO student (name, email) VALUES (%s, %s);""", (_name, _email))
-            conn.commit()
-            return json.dumps({'message':'User created successfully !'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
-
+    return res.read()
 
 
 @app.route('/login')
 def login():
-    callback=url_for('authorized', _external=True)
+    callback=url_for('authorized')
     return google.authorize(callback=callback)
+
 
 @app.route('/login/authorized')
 @google.authorized_handler
@@ -88,19 +62,12 @@ def authorized(resp):
         session['access_token'] = access_token, ''
         return redirect(url_for('index'))
 
+
 @google.tokengetter
 def get_access_token():
     return session.get('access_token')
 
-@app.route('/singlecourse', methods=['GET'])
-def getCourseInfo():
-    return json.dumps({'course':'<span>'+request.args.get('course')+'</span>'})
 
 
-@app.route('/multiplecourses', methods=['GET'])
-def compareCourses():
-    return json.dumps({'course':'<span>'+request.args.get('course')+'</span>'})
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
